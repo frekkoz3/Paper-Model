a190db36e6a9566a8c2d726a49a828305aade7c6
Rheatrias <francescobredariol03@gmail.com>
2026-04-30 23:07:56 +0200

    Updated the footer a lot. Tomorrow to implement the logos and we are good to go.

--- TODO.md
+++ TODO.md
@@ -1,7 +1,7 @@
 # TODO
 
 [ ] Work on the augmentation : next-page noise
-[ ] Work on the HEADER and FOOTER!!!
+[ ] Work on the HEADER and FOOTER :  WE NEED JUST TO ADD SOME LOGOS HERE AND THERE AND WE ARE GOOD
 
 [ ] To write all the README
 [ ] Find the DOCLAYOUT fine tuning routine
--- src/generator/header.py
+++ src/generator/header.py
@@ -30,28 +30,88 @@ class Header(Component):
         """
         fake = Faker(['en_US', 'it_IT'])
         self.name = fake.sentence(nb_words=2, variable_nb_words=True)
+        self.subtitle = fake.sentence(nb_words=4, variable_nb_words=True)
         self.date = f"{self.anchor.date.strftime('%B %d, %Y')}"
+        self.price = f"{round(random.random(), 1)}0 {random.choice(["£", "$", "¥", "€"])}"
         self.date_type = random.choice(['center', 'top-right', 'bottom-right', 'none'])
-        self.font_size = random.randint(28, 64)
-        pass
+        
+        
 
     def render(self):
-        formatted_date = f"<center> {self.date} </center>" # to think how to place elsewhere
 
-        hr_h = random.randint(1, 5)
-        upper_lines = ['<div class="line"></div>' for _ in range (random.randint(0, 2))] if random.random() < 0.5 else ['<hr>']
-        lower_lines = ['<div class="line"></div>'  for _ in range (random.randint(0, 2))]
+        font_family = random.choice(self.anchor.page_cfg["fonts"])
+        
+        font_size = random.randint(8, 14)
+        title_size = random.randint(28, 64)
+        subtitle_size = random.uniform(0.2, 0.5)*title_size
+
+        # Upper section
+        upper_lines = ['<div class="line"></div>' for _ in range (random.randint(1, 3))]
+        div_top = ""
+        if random.random() < self.anchor.header_cfg["upper probability"]:
+            div_top = f"""
+                <div class="header-top">
+                {self.price} • {random.randint(18908, 37827)} • {self.date}
+                {''.join(upper_lines) if upper_lines else ''}
+            </div>
+            """
+
+        # Lower section
+        lower_lines = ['<div class="line"></div>'  for _ in range (random.randint(1, 3))]
+        div_bottom = ""
+        if random.random() < self.anchor.header_cfg["lower probability"]:
+            div_bottom = f"""{''.join(lower_lines) if lower_lines else ''}"""
+
+        # Logo -> THIS MUST BE IMPLEMENTED -> could be a subclass of banner
+        # look at this https://github.com/msn199959/Logo-2k-plus-Dataset
+        logo_sx = f"""
+            <div class="logo left">
+            {"***" if random.random() < self.anchor.header_cfg["logo probability"] else ""}
+            </div>
+        """
+        logo_dx = f"""
+            <div class="logo right">
+            {"***" if random.random() < self.anchor.header_cfg["logo probability"] else ""}
+            </div>
+        """
+
+        # Subtitle
+        subt = ""
+        if random.random() < self.anchor.header_cfg["subtitle"]:
+            subt = f"""
+            <div class="newspaper-subtitle">
+                {self.subtitle}
+            </div>
+            """
 
         return f"""
         <div class="header"
         style="--header-padding:{self.padding}px;
-               --header-font-size:{self.font_size}px;
+               --header-font-family:{font_family};
+               --header-font-size:{font_size}px;
+               --header-title-font-size:{title_size}px;
+               --header-subtitle-font-size:{subtitle_size}px;
                --header-h:{self.height}px;
-               --header-w:{self.width}px;
-               --hr-height : {hr_h}px;">
-            {''.join(upper_lines) if upper_lines else ''}
-            <center> {self.name} </center> 
-            <div class="date"> {self.date} </div>
-            {''.join(lower_lines) if lower_lines else ''}
+               --header-w:{self.width}px;">
+
+            {div_top}
+            
+            <div class="masthead">
+
+                {logo_sx}
+                
+                <div class="title-group">
+                    <div class="newspaper-title">
+                        {self.name}
+                    </div>
+                    {subt}
+                </div>
+
+                {logo_dx}
+
+            </div>
+
+            {div_bottom}
+            
         </div>
         """
