9ffb3a22fd8f2caa61a7531c962684e39c1b7dcb
Rheatrias <francescobredariol03@gmail.com>
2026-05-07 15:06:58 +0200

    Changed some paramters in the rt_detr_routine

--- src/fine_tuning/rt_detr_routine.py
+++ src/fine_tuning/rt_detr_routine.py
@@ -22,7 +22,7 @@ if __name__ == '__main__':
 
     model.train(
         data="configs/data.yaml",
-        epochs=40,
+        epochs=100,
         imgsz=1024,
         batch=16,
         lr0=0.001,
