1bd8e8aeaa6a6f84662422fafcd44c767c35810a
Rheatrias <francescobredariol03@gmail.com>
2026-05-08 10:48:35 +0200

    Mad proof

--- src/generator/data_gen.py
+++ src/generator/data_gen.py
@@ -10,10 +10,8 @@ r"""
 
     A simple rule-based model to generate realistical newspapers' pages for the training of the YOLO-Layout model.
 """
-print("Hi")
-
 from src.generator.page import Page, get_labels_from_page
-#from playwright.sync_api import sync_playwright
+from playwright.sync_api import sync_playwright
 from src.augmenter.augment import augment_page
 
 from pathlib import Path
@@ -115,10 +113,17 @@ def generate_train_and_validation_set(
     val_lbl_path.mkdir(parents=True, exist_ok=True)
     val_html_path.mkdir(parents=True, exist_ok=True)
 
+    with open("debug.txt", "+a") as f:
+        f.write("Hi\n")
+
     try:
 
         if verbose == 1:
             print("Starting playwright")
+
+        with open("debug.txt", "+a") as f:
+            f.write("Again\n")
+
 
         with sync_playwright() as p:
 
